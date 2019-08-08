3d1f83e530513ed1672ce9ac124bf67000e394ce
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-08-08 08:26:42 -0500

    created modals for login/register

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,6 +15,25 @@ module.exports = function(app) {
     });
   });
 
+  app.post("/api/users", function(req, res) {
+    console.log(req.body)
+    db.User.create(req.body).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  app.get("/api/users/:email", function(req, res) {
+    console.log("req.params: ", req.params)
+    db.User.findOne({
+      where:{
+        email: req.params.email
+      }
+    }).then(function(dbUser) {
+      console.log("dbUser: ", dbUser)
+      res.json(dbUser);
+    });
+  });
+
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
