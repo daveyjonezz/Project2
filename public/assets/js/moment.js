3e40e9bd038d1708b5fa754a8e58cfde94b9bcd5
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-08-14 14:13:07 -0500

    weather API clean up and reformat in multiple docs

--- public/assets/js/moment.js
+++ public/assets/js/moment.js
@@ -1,25 +1,52 @@
+// var currentTime = new Date().getHours();
+// //morning image for background
+// console.log(currentTime)
+// if (5 <= currentTime && currentTime < 10) {
+    
+//         $("#background").attr("src","/assets/images/backgrounds/calm-morning.png");
+// }
+
+// //day image for background
+// else if (10 <= currentTime && currentTime < 16){
+   
+//         $("#background").attr("src","/assets/images/backgrounds/calm-day.png");
+   
+// }
+// //evening image for background
+// else if (16 <= currentTime && currentTime < 19) {
+    
+//         $("#background").attr("src","/assets/images/backgrounds/calm-evening.png");
+// }
+// //night image for background
+// else  {
+    
+//         $("#background").attr("src","/assets/images/backgrounds/calm-night.png");
+//      console.log("evening")
+// }
+
 var currentTime = new Date().getHours();
 //morning image for background
 console.log(currentTime)
+currentTime = 6
 if (5 <= currentTime && currentTime < 10) {
     
-        $("#background").attr("src","/assets/images/backgrounds/calm-morning.png");
+        $("#background").css({"background-image": url("/assets/images/backgrounds/calm-day.png"));
 }
 
 //day image for background
 else if (10 <= currentTime && currentTime < 16){
    
-        $("#background").attr("src","/assets/images/backgrounds/calm-day.png");
+        $("#background").css("src","/assets/images/backgrounds/calm-day.png");
    
 }
 //evening image for background
 else if (16 <= currentTime && currentTime < 19) {
     
-        $("#background").attr("src","/assets/images/backgrounds/calm-evening.png");
+        $("#background").css("src","/assets/images/backgrounds/calm-evening.png");
 }
 //night image for background
 else  {
     
-        $("#background").attr("src","/assets/images/backgrounds/calm-night.png");
+        $("#background").css("src","/assets/images/backgrounds/calm-night.png");
      console.log("evening")
 }
