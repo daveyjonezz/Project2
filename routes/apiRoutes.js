03c939ae5a5d0d157cfc0baf6aceb812482f68e4
Sooze16 <slkpes2002@yahoo.com>
2019-08-15 18:08:45 -0500

    fixed weather modal upon login to only show current conditions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,73 +2,68 @@ var db = require("../models");
 var weather = require("weather-js");
 var twilioClient = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-module.exports = function (app) {
+module.exports = function(app) {
     // Get all examples
-    app.get("/api/examples", function (req, res) {
-        db.Example.findAll({}).then(function (dbExamples) {
+    app.get("/api/examples", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
             res.json(dbExamples);
         });
     });
 
     // Create a new example
-    app.post("/api/examples", function (req, res) {
-        db.Example.create(req.body).then(function (dbExample) {
+    app.post("/api/examples", function(req, res) {
+        db.Example.create(req.body).then(function(dbExample) {
             res.json(dbExample);
         });
     });
 
     // register new user
-    app.post("/api/users", function (req, res) {
+    app.post("/api/users", function(req, res) {
         console.log(req.body)
 
         console.log(req.body.email)
         db.User.findOne({
-            where:
-            {
+            where: {
                 email: req.body.email
             }
-        }).then(function (existingUser) {
-            if(!existingUser){
-                console.log('user NOT exists'); 
+        }).then(function(existingUser) {
+            if (!existingUser) {
+                console.log('user NOT exists');
                 db.User.create(req.body).then(function(dbUser) {
                     res.cookie("zipCode", dbUser.location)
-                    res.render("userPage" , dbUser.dataValues);
+                    res.render("userPage", dbUser.dataValues);
                 });
-            }
-            else{
-                console.log('user DOES exists'); 
+            } else {
+                console.log('user DOES exists');
                 res.send("404")
             }
         });
     });
     // Login User
-    app.get("/api/users/:email", function (req, res) {
+    app.get("/api/users/:email", function(req, res) {
         console.log("req.params: ", req.params)
 
         db.User.findOne({
-            where:
-            {
+            where: {
                 email: req.params.email
             }
-        }).then(function (dbUser)
-         {
+        }).then(function(dbUser) {
             if (dbUser !== null) {
-            console.log('=========================');
-            console.log(dbUser.dataValues)
-            console.log('=========================');
-            res.cookie("zipCode", dbUser.location);
-            res.render("userPage", dbUser.dataValues);
-            }
-            else {
-                console.log("HEY",dbUser)
+                console.log('=========================');
+                console.log(dbUser.dataValues)
+                console.log('=========================');
+                res.cookie("zipCode", dbUser.location);
+                res.render("userPage", dbUser.dataValues);
+            } else {
+                console.log("HEY", dbUser)
                 res.send("404")
             }
         });
     });
 
     // Delete an example by id
-    app.delete("/api/examples/:id", function (req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+    app.delete("/api/examples/:id", function(req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
             res.json(dbExample);
         });
     });
@@ -77,20 +72,22 @@ module.exports = function (app) {
         // we can get the zipcode from the cookie
 
 
-        weather.find({ search: req.params.zipcode, degreeType: 'F' }, function (err, result) {
+        weather.find({ search: req.params.zipcode, degreeType: 'F' }, function(err, result) {
             if (err) console.log(err);
+
+            console.log("current:", result)
             res.json(result);
         });
     });
 
-    app.get("/send/:phoneNum", function (req, res) {
+    app.get("/send/:phoneNum", function(req, res) {
         console.log("hello req.params: ", req.params);
         console.log('#', process.env.TWILIO_NET_PHONE_NUMBER);
-        
+
         // process.env.TWILIO_NET_PHONE_NUMBER
-       // console.log("hello twilio ", process.env.YING_DEST_PHONE_NUMBER, process.env.TWILIO_NET_PHONE_NUMBER)
-    //    }).then(function (dbUser) {
-    //    console.log(dbUser);
+        // console.log("hello twilio ", process.env.YING_DEST_PHONE_NUMBER, process.env.TWILIO_NET_PHONE_NUMBER)
+        //    }).then(function (dbUser) {
+        //    console.log(dbUser);
         twilioClient.messages.create({
             to: `'+1${req.params.phoneNum}'`,
             from: process.env.TWILIO_NET_PHONE_NUMBER,
